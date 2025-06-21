a212bc3e9435b1294cced5ab3109df282770ccc4
Ashish Kumar Mishra <amishrahsm@gmail.com>
2025-06-21 20:03:25 +0530

    submenu

--- src/components/header/Navigation/index.jsx
+++ src/components/header/Navigation/index.jsx
@@ -13,11 +13,14 @@ function Navigation() {
     setIsOpenPannel(value)
   }
   return (
-    <>    
+    <>
       <nav className="py-2">
         <div className="container flex items-center justify-end gap-8">
           <div className="col_1 w-[20%]">
-            <Button className="!text-black gap-2 !w-full !font-[500]" onClick={openCategoryPannel}>
+            <Button
+              className="!text-black gap-2 !w-full !font-[500]"
+              onClick={openCategoryPannel}
+            >
               <RiMenu2Fill className="!text-[18px]" />
               Shop By Categories
               <LiaAngleDownSolid className="text-[13px] ml-auto font-bold" />
@@ -25,17 +28,100 @@ function Navigation() {
           </div>
 
           <div className="col_2 w-[60%]">
-            <ul className="flex items-center gap-4">
+            <ul className="flex items-center gap-4 nav">
               <li className="list-none">
                 <Link to="/" className="link transition font-[500] text-[14px]">
                   <Button>Home</Button>
                 </Link>
               </li>
 
-              <li className="list-none">
-                <Link to="/" className="link transition font-[500] text-[14px]">
+              <li className="list-none relative">
+                <Link
+                  to="/"
+                  className="link transition !font-[500] !text-[14px]"
+                >
                   <Button>Fashion</Button>
                 </Link>
+
+                <div className="submenu absolute top-[120%] left-[0%] min-w-[200px] bg-white shadow-md opacity-0 transition-all">
+                  <ul className="w-full">
+                    <li className="list-none w-full !relative">
+                      <Link to="/" className="w-full">
+                        <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start">
+                          Man
+                        </Button>
+
+                        <div className="submenu !absolute top-[60%] left-[100%] min-w-[200px] bg-white shadow-md opacity-0 transition-all">
+                          <ul className="w-full">
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start">
+                                  Jeans
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start">
+                                  Tshirt
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start">
+                                  Shoes
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none">
+                              <Link to="/" className="w-full">
+                                <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start">
+                                  Watch
+                                </Button>
+                              </Link>
+                            </li>
+                            <li className="list-none w-full">
+                              <Link to="/" className="w-full">
+                                <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start">
+                                  Pents
+                                </Button>
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start">
+                          Women
+                        </Button>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start">
+                          Kids
+                        </Button>
+                      </Link>
+                    </li>
+                    <li className="list-none">
+                      <Link to="/" className="w-full">
+                        <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start">
+                          Girls
+                        </Button>
+                      </Link>
+                    </li>
+                    <li className="list-none w-full">
+                      <Link to="/" className="w-full">
+                        <Button className="!text-[rgba(0,0,0,0.8)] w-full !text-left !justify-start">
+                          Boys
+                        </Button>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </li>
 
               <li className="list-none">
@@ -90,7 +176,10 @@ function Navigation() {
           </div>
         </div>
       </nav>
-      <CategoryPannel openCategoryPannel={openCategoryPannel} isOpenPannel={isOpenPannel}/>
+      <CategoryPannel
+        openCategoryPannel={openCategoryPannel}
+        isOpenPannel={isOpenPannel}
+      />
     </>
   );
 }
